8157495e037ccafc64cff52a2e8db6c65db20bf0
Premalatha <rpremalatha@nmsworks.co.in>
2024-08-28 09:55:04 +0530

    file commited

---
+++ premalatha/src/main/java/in/co/nmsworks/HelloWorld.java
@@ -0,0 +1,12 @@
+package in.co.nmsworks;
+
+public class HelloWorld {
+    public void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        HelloWorld h = new HelloWorld();
+        h.printHelloWorld();
+    }
+}
